36e0c3933d21d68235385d29c83443b0bc18d31a
KingNelx <tapiajonel606@gmail.com>
2023-05-11 14:37:07 +0800

    added images for users

--- src/Components/UsersLogin.jsx
+++ src/Components/UsersLogin.jsx
@@ -1,9 +1,47 @@
 const UsersLogin = () => {
-    return ( 
+    return (
         <div>
-            
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        <div class="card" style={
+                            { width: "18rem" }
+                        }>
+                            <img src="..." class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">STUDENT</h5>
+                                <a href="#" class="btn btn-primary">Sign in</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card" style={
+                            { width: "18rem" }
+                        }>
+                            <img src="..." class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">CLERK</h5>
+                                <a href="#" class="btn btn-primary">Sign in</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card" style={
+                            { width: "18rem" }
+                        }>
+                            <img src="..." class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">ADMIN</h5>
+                                <a href="#" class="btn btn-primary">Sign in</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-     );
+    );
 }
- 
+
 export default UsersLogin;

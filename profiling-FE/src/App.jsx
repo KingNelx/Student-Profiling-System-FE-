7aada0b5ba960602cc120848bf9272139878f58e
KingNelx <tapiajonel606@gmail.com>
2023-06-12 00:16:24 +0800

    added route for signing up

--- profiling-FE/src/App.jsx
+++ profiling-FE/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './Pages/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import UserLogIn from './Pages/UserLogIn';
+import AdminSignIn from './Admin/AdminSignIn';
+import AdminSignUp from './Admin/AdminSignUp';
 
 const App = () => {
     return (
@@ -11,6 +13,8 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/userLogIn" element={<UserLogIn />} />
+                    <Route path="/adminSignIn" element={<AdminSignIn />} />
+                    <Route path="/adminSignUp" element={<AdminSignUp />} />
                 </Routes>
             </Router>
         </div>

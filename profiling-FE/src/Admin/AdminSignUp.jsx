1cd5e18f0df784a42031ab72169fbc5916c3491f
KingNelx <tapiajonel606@gmail.com>
2023-08-18 20:23:56 +0800

    sign up admin 404

--- profiling-FE/src/Admin/AdminSignUp.jsx
+++ profiling-FE/src/Admin/AdminSignUp.jsx
@@ -3,10 +3,42 @@ import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/esm/Container';
 import Button from 'react-bootstrap/Button';
 import NavTitle from '../Components/NavTitle';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
+import axios from "axios"
+import { useState } from 'react';
 
 const AdminSignUp = () => {
+
+    const navigate = useNavigate()
+
+    const [admin, setAdmin] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        userName: "",
+        password: ""
+    })
+
+    const { firstName, lastName, email, userName, password } = admin
+
+    const onInputChange = (e) => {
+        const { name, value } = e.target
+        setAdmin({ ...admin, [name]: value.toUpperCase() })
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await axios.post("http://localhost:8080/admin/create-acount", admin)
+            if (response.status === 200) {
+                navigate("/adminHome")
+                window.location.reload()
+            }
+        } catch (error) {
+            alert(" ERROR MESSAGE: " + error)
+        }
+    }
 
     const loading = () => {
         let timerInterval
@@ -38,27 +70,45 @@ const AdminSignUp = () => {
             <>
                 {<NavTitle />}
                 <Container className='mt-5'>
-                    <Form>
+                    <Form onSubmit={(e) => onSubmit(e)}>
                         <FloatingLabel
                             controlId="floatingInput"
                             label="First name"
                             className="mb-4"
                         >
-                            <Form.Control type="text" />
+                            <Form.Control
+                                type={"text"}
+                                value={firstName}
+                                name="firstName"
+                                required
+                                onChange={(e) => onInputChange(e)}
+                            />
                         </FloatingLabel>
                         <FloatingLabel
                             controlId="floatingInput"
                             label="Last name"
                             className="mb-4"
                         >
-                            <Form.Control type="text" />
+                            <Form.Control
+                                type={"text"}
+                                value={lastName}
+                                name="lastName"
+                                required
+                                onChange={(e) => onInputChange(e)}
+                            />
                         </FloatingLabel>
                         <FloatingLabel
                             controlId="floatingInput"
                             label="Email"
                             className="mb-4"
                         >
-                            <Form.Control type="email" />
+                            <Form.Control
+                                type={"email"}
+                                value={email}
+                                name="email"
+                                required
+                                onChange={(e) => onInputChange(e)}
+                            />
                         </FloatingLabel>
 
                         <FloatingLabel
@@ -66,16 +116,28 @@ const AdminSignUp = () => {
                             label="User name"
                             className="mb-4"
                         >
-                            <Form.Control type="text" />
+                            <Form.Control
+                                type={"text"}
+                                value={userName}
+                                name="userName"
+                                required
+                                onChange={(e) => onInputChange(e)}
+                            />
                         </FloatingLabel>
                         <FloatingLabel
                             controlId="floatingInput"
                             label="Password"
                             className="mb-4"
                         >
-                            <Form.Control type="password" />
+                            <Form.Control
+                                type={"password"}
+                                value={password}
+                                name="password"
+                                required
+                                onChange={(e) => onInputChange(e)}
+                            />
                         </FloatingLabel>
-                        <Button variant='outline-primary'>Sign Up</Button> {' '}
+                        <Button variant='outline-primary' type="submit">Sign Up</Button> {' '}
                         <Link to="/adminHome"><Button variant='outline-success' onClick={loading}>Go Back</Button></Link>
                     </Form>
                 </Container>

63519a5d3a8a1901c690d5d244e2c5695432830e
KingNelx <tapiajonel606@gmail.com>
2023-09-02 20:53:24 +0800

    added student form components

--- profiling-FE/src/Students/AddStudent.jsx
+++ profiling-FE/src/Students/AddStudent.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import FloatingLabel from 'react-bootstrap/FloatingLabel'
 import Form from 'react-bootstrap/Form'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row';
 
 const AddStudent = () => {
 
@@ -49,18 +51,58 @@ const AddStudent = () => {
     const { firstName, lastName, gender, age, elementarySchool, elementarySchoolYearStarted, elementarySchoolYearEnded, elementarySchoolAddress,
         highSchool, highSchoolYearStarted, highSchoolYearEnded, highSchoolAddress, seniorHighSchool, seniorHighYearStarted, seniorHighYearEnded, seniorHighAddress,
         college, collegeYearStarted, collegeYearEnded, collegeAddress, fathersFirstName, fathersLastName, fathersAddress, fathersCivilStatus, fathersAge, fathersContactNumber, fathersOccupation, fathersHighestEducation,
-        mothersFirstName, mothersLastName, mothersAddress, mothersCivilStatus, mothersAge, 
+        mothersFirstName, mothersLastName, mothersAddress, mothersCivilStatus, mothersAge,
     } = student
 
     return (
         <div>
-            <FloatingLabel
-                controlId="floatingInput"
-                label="Email address"
-                className="mb-3"
-            >
-                <Form.Control type="email" placeholder="name@example.com" />
-            </FloatingLabel>
+            <Form>
+                <Row>
+                    <p className="text-center fw-bold"> PERSONAL DETAILS </p>
+
+                    <Col>
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label="FIRSTNAME"
+                            className="mb-3"
+                        >
+                            <Form.Control type={"text"} placeholder='Firstname' />
+                        </FloatingLabel>
+                    </Col>
+
+                    <Col>
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label="LASTNAME"
+                            className="mb0-3"
+                        >
+                            <Form.Control type={"text"} placeholder="Lastname" />
+                        </FloatingLabel>
+                    </Col>
+
+                    <Col>
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="GENDER"
+                        className="mb-3"
+                    >
+                        <Form.Control type={"text"} placeholder="Gender" />
+                    </FloatingLabel>
+                    </Col>
+
+                    <Col>
+                    <FloatingLabel
+                        controlId="floatingInput"
+                        label="AGE"
+                        className="mb-3"
+                    >
+                        <Form.Control type={"text"} placeholder="Age" />
+                    </FloatingLabel>
+                    </Col>
+
+                </Row>
+                <Button type="submit" variant="outline-primary">REGISTER</Button>
+            </Form>
 
         </div>
     );

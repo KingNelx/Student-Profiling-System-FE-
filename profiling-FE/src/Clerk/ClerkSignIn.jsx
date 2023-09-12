7b54a7702afd3039322f16d2d71789f662bf3229
KingNelx <tapiajonel606@gmail.com>
2023-09-12 19:17:36 +0800

    REMOVE window location reload

--- profiling-FE/src/Clerk/ClerkSignIn.jsx
+++ profiling-FE/src/Clerk/ClerkSignIn.jsx
@@ -7,8 +7,29 @@ import { Link } from 'react-router-dom'
 import TopNav from "../Components/TopNav";
 import FloatingLabel from 'react-bootstrap/esm/FloatingLabel'
 import Loading from '../Components/Loading'
+import { useState } from 'react'
 
 const ClerkSignIn = () => {
+
+    const [clerkLog, setClerkLog] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        userName: "",
+        password: ""
+    })
+
+    const { firstName, lastName, email, userName, password } = clerkLog
+
+    const updateFormField = (e) => {
+        const { name, value } = e.target
+        setClerkLog({ ...clerkLog, [name]: value.toUpperCase() })
+    }
+
+    const submitForm = (e) => {
+        e.preventDefault()
+    }
+
     return (
         <div>
             <TopNav />
@@ -50,7 +71,7 @@ const ClerkSignIn = () => {
                         <Button variant='outline-primary'>SUBMIT</Button>{" "}
                         <Link to='/clerkSignUp' variant="outline-primary" onClick={Loading}>No Account? Sign up.</Link>
                     </Container>
-                    
+
                 </Form>
             </div>
         </div>

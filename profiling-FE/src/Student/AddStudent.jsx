1a32114a76da60056808bcbb383d265e53b99a81
KingNelx <tapiajonel606@gmail.com>
2023-09-21 09:27:04 +0800

    BINDED BACK END PROPERTIES TO THE USE STATE HOOKS

--- profiling-FE/src/Student/AddStudent.jsx
+++ profiling-FE/src/Student/AddStudent.jsx
@@ -2,17 +2,94 @@ import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
+import FloatingLabel from 'react-bootstrap/esm/FloatingLabel'
+import { useState } from 'react'
+
 
 const AddStudent = () => {
-    return ( 
+
+    const [student, studentData] = useState({
+        studentID: "",
+        firstName: "",
+        lastName: "",
+        middleInitial: "",
+        dateOfBirth: "",
+        gender: "",
+        contactNumber: "",
+        emailAddress: "",
+        address: "",
+        educationLevel: "",
+        schoolName: "",
+        gradeLevel: "",
+        dateStarted: "",
+        expectedGraduationYear: "",
+        schoolAddress: "",
+        fathersFullName: "",
+        fathersAge: "",
+        fathersAddress: "",
+        fathersContactNumber: "",
+        fathersCivilStatus: "",
+        fathersOccupation: "",
+        fathersEducationLevel: "",
+        fathersDateOfBirth: "",
+        mothersFullName: "",
+        motherssAge: "",
+        mothersAddress: "",
+        mothersContactNumber: "",
+        mothersCivilStatus: "",
+        mothersOccupation: "",
+        mothersEducationLevel: "",
+        mothersDateOfBirth: "",
+    })
+
+
+    const { studentID, firstName, lastName, middleInitial, dateOfBirth, gender, contactNumber, emailAddress, address,
+        educationLevel, schoolName, gradeLevel, dateStarted, expectedGraduationYear, schoolAddres,
+        fathersFullName, fathersAge, fathersAddress, fathersContactNumber, fathersCivilStatus, fathersOccupation, fathersEducationLevel, fathersDateOfBirth,
+        mothersFullName, motherssAge, mothersAddress, mothersContactNumber, mothersCivilStatus, mothersOccupation, mothersEducationLevel, mothersDateOfBirth,
+    } = student
+
+    return (
         <div>
             <Form>
-                <Container>
-                    
+                <Container className='text-center'>
+                    <Row>
+                        <Col>
+                            <FloatingLabel
+                                controlId="floatingInput"
+                                className='mb-3'
+                                label='Student ID'
+                            >
+                                <Form.Control type={"text"} />
+                            </FloatingLabel>
+                        </Col>
+
+                        <Col>
+                            <FloatingLabel
+                                controlId="floatingInput"
+                                className='mb-3'
+                                label='Student ID'
+                            >
+                                <Form.Control type={"text"} />
+                            </FloatingLabel>
+                        </Col>
+
+                        <Col>
+                            <FloatingLabel
+                                controlId="floatingInput"
+                                className='mb-3'
+                                label='Student ID'
+                            >
+                                <Form.Control type={"text"} />
+                            </FloatingLabel>
+                        </Col>
+
+
+                    </Row>
                 </Container>
             </Form>
         </div>
-     );
+    );
 }
- 
+
 export default AddStudent;
--- profiling-FE/src/Student/AddStudentModal.jsx
+++ profiling-FE/src/Student/AddStudentModal.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import AddStudent from './AddStudent';
 
 const AddStudentModal = () => {
 
@@ -19,19 +20,18 @@ const AddStudentModal = () => {
                 onHide={handleClose}
                 backdrop="static"
                 keyboard={false}
+                size="xl"
             >
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal title</Modal.Title>
+                    <Modal.Title>ADD STUDENT</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    I will not close if you click outside me. Don not even try to press
-                    escape key.
+                    <AddStudent />
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
                         Close
                     </Button>
-                    <Button variant="primary">Understood</Button>
                 </Modal.Footer>
             </Modal>
         </div >

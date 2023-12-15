cbab3fc8b58e99ec9ab0b63f5cc60e938aef28e2
KingNelx <tapiajonel606@gmail.com>
2023-12-15 10:03:31 +0800

    added admin folders and SWAL

---
+++ profiling-FE/src/Admin/AdminPage.jsx
@@ -0,0 +1,27 @@
+import Card from "react-bootstrap/Card"
+import Form from "react-bootstrap/Form"
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import Button from "react-bootstrap/Button"
+
+const AdminPage = () => {
+    return (
+        <Container>
+            <Card>
+                <Card.Text>
+                    <Row>
+                        <Col>
+                            TEST
+                        </Col>
+                        <Col>
+                            TEST
+                        </Col>
+                    </Row>
+                </Card.Text>
+            </Card>
+        </Container>
+    );
+}
+
+export default AdminPage;
--- profiling-FE/src/App.jsx
+++ profiling-FE/src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Components/Pages/Home';
+import AdminPage from './Admin/AdminPage';
 
 const App = () => {
     return (
-        <Router>
-            <Routes>
-                <Route />
-            </Routes>
-        </Router>
+        <div className="bg-main">
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/adminPage" element={<AdminPage />} />
+                </Routes>
+            </Router>
+        </div>
     );
 }
 

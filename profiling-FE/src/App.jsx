43fc2496f3efe6653d969f8fd57526405e20841d
KingNelx <tapiajonel606@gmail.com>
2024-01-26 21:23:11 +0800

    reset

--- profiling-FE/src/App.jsx
+++ profiling-FE/src/App.jsx
@@ -1,14 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./Components/Pages/Home";
-import AdminPage from "./Admin/AdminPage";
-import AdminHome from "./Admin/AdminHome";
-import AdminSignUp from "./Admin/AdminSignUp";
-import FreshmanPage from "./Components/Pages/FreshmanPage";
-import SophomorePage from "./Components/Pages/SophomorePage";
-import JuniorPage from "./Components/Pages/JuniorPage";
-import SeniorPage from "./Components/Pages/SeniorPage";
+import Home from "./Pages/Home";
+
 
 
 const App = () => {
@@ -16,14 +10,7 @@ const App = () => {
         <div className="bg-main">
             <Router>
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/adminPage" element={<AdminPage />} />
-                    <Route path="/adminHome" element={<AdminHome />} />
-                    <Route path="/adminSignUp" element={<AdminSignUp />} />
-                    <Route path="/freshman" element={<FreshmanPage />} />
-                    <Route path="/sophomore" element={<SophomorePage />} />
-                    <Route path="/junior" element={<JuniorPage />} />
-                    <Route path="/senior" element={<SeniorPage />} />
+                 <Route path="/" element={<Home />} />
                 </Routes>
             </Router>
         </div>
---
+++ profiling-FE/src/Components/HomeNavBar.jsx
@@ -0,0 +1,9 @@
+const HomeNavBar = () => {
+    return ( 
+        <div>
+            
+        </div>
+     );
+}
+ 
+export default HomeNavBar;

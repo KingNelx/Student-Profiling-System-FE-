01d34a0776c7e701fe140495967aa250b4b233a0
KingNelx <tapiajonel606@gmail.com>
2024-03-14 14:29:25 +0800

    reset

--- profiling-FE/src/App.jsx
+++ profiling-FE/src/App.jsx
@@ -1,23 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import HomeNavBar from "./Pages/HomeNavBar";
-import SignUpForm from "./Components/SignUpForm"
-import AdminHome from "./Admin/AdminHome";
-
 
 const App = () => {
     return (
-        <div className="bg-main ">
-            <Router>
-                <Routes>
-                    <Route path="/" element={<HomeNavBar />} />
-                    <Route path="/sign-up" element={<SignUpForm />} />
-                    <Route path="/admin-home" element={<AdminHome />} />
-                </Routes>
-            </Router>
+        <div>
+
         </div>
     );
-};
+}
 
 export default App;
